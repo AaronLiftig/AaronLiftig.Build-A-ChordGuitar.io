15c39c1b6fa712e4280b942ba552d24311fc21b1
aaron <aaronliftig@gmail.com>
2022-05-10 03:33:23 +1000

    Add mini-guitar to guitar chord mini-screen

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ const tuningList = [ "Standard", "Drop D", "DADGAD", "Double Drop D", "Open D",
 
 function addFretMarkId(fretDiv, fretNum) {
     if ([3, 5, 7, 9, 15, 17, 19, 21].includes(fretNum)) {
-        fretDiv.id = "single-fret-mark";
+        fretDiv.className += " single-fret-mark";
     } else if ([12, 24].includes(fretNum)) {
-        fretDiv.id = "double-fret-mark";
+        fretDiv.className += " double-fret-mark";
     } else if (fretNum == 0) {
-        fretDiv.id = "nut";
+        fretDiv.className += " nut";
     }
     return fretDiv;
 }
@@ -62,16 +62,21 @@ function getAccidentalFromFretText(fretData, {accidentalType="#"}) {
 
 function getGuitarParameters() {
     const tuningParam = document.getElementById("guitar-tuning-dropdown").value;
-    const fretNumParam = document.getElementById("guitar-fretNum-dropdown").value;
+    const numOfFretsParam = document.getElementById("guitar-numOfFrets-dropdown").value;
     const accidentalTypeParam = document.getElementById("guitar-accidentalType-dropdown").value;
 
-    return {tuningParam, fretNumParam, accidentalTypeParam};
+    return {tuningParam, numOfFretsParam, accidentalTypeParam};
 }
 
 
-function getGuitar(tuning, fretNum) {
-    const guitarApp = new GuitarApp({tuning: tuning, numOfFrets: fretNum});
+function getGuitar(tuning, numOfFrets) {
+    const guitarApp = new GuitarApp({tuning: tuning, numOfFrets: numOfFrets});
     return guitarApp.yourGuitar.guitar;
+}
+
+function getMiniScreen(tuning, numOfFrets, stringNum, fretNum) {
+    const guitarApp = new GuitarApp({tuning: tuning, numOfFrets: numOfFrets});
+    return guitarApp.getMiniScreen(stringNum, fretNum);
 }
 
 
@@ -88,8 +93,8 @@ function getGuitarAxis(guitarElement) {
 
 function populateGuitarAxis(guitarAxisRow, b) {
     let axisRowFret = document.createElement("div");
-    axisRowFret = addFretMarkId(axisRowFret, b);
     axisRowFret.className = "guitar-fret-axis";
+    axisRowFret = addFretMarkId(axisRowFret, b);
     let axisRowFretP = document.createElement("p");
     let text = document.createTextNode(b);
     axisRowFretP.append(text);
@@ -99,14 +104,17 @@ function populateGuitarAxis(guitarAxisRow, b) {
 
 
 function createFret(guitarAxisRow, accidentalType, guitarString, a, b) {
+    let fretNum = guitarString[b][0][1];
+    
     if (a === 0) {
-        populateGuitarAxis(guitarAxisRow, b);
+        populateGuitarAxis(guitarAxisRow, fretNum);
     }
 
     let guitarFret = guitarString[b];
     let fret = document.createElement("div");
-    fret = addFretMarkId(fret, b)
     fret.className = "guitar-fret";
+    fret = addFretMarkId(fret, fretNum);
+    fret.id = `${a},${b}`;
     fret.dataset.modalTarget = "#modal";
     let fretP = document.createElement("p");
     
@@ -114,7 +122,7 @@ function createFret(guitarAxisRow, accidentalType, guitarString, a, b) {
     
     fretP.append(fretText);
     fret.append(fretP);
-    return fret
+    return fret;
 }
 
 
@@ -154,6 +162,11 @@ function addOpenEventListenersToGuitar() {
     const openModalButtons = document.querySelectorAll("[data-modal-target]");
     openModalButtons.forEach(button => {
         button.addEventListener("click", () => {
+            let [stringNum, fretNum] = button.id.split(",");
+            stringNum = parseInt(stringNum);
+            fretNum = parseInt(fretNum);
+            updateGuitarDiv("mini-screen", {stringNum: stringNum, fretNum: fretNum})
+
             const modal = document.querySelector(button.dataset.modalTarget);
             openModal(modal);
         })
@@ -161,22 +174,34 @@ function addOpenEventListenersToGuitar() {
 }
 
 
-function updateGuitarDiv() {
+function updateGuitarDiv(screen, {stringNum = null, fretNum = null}) {
     params = getGuitarParameters();
     let tuning = params.tuningParam;
-    let fretNum = parseInt(params.fretNumParam);
+    let numOfFrets = parseInt(params.numOfFretsParam);
     let accidentalType = params.accidentalTypeParam;
 
-    let guitar = getGuitar(tuning, fretNum);
+    let guitar;
+    let guitarElement;
+    if (screen === "main-guitar") {
+        guitar = getGuitar(tuning, numOfFrets);
+        guitarElement = document.getElementById("main-guitar");
+    } else {
+        guitar = getMiniScreen(tuning, numOfFrets, stringNum, fretNum)
+        mainGuitar = getGuitar(tuning, numOfFrets);
+        guitarElement = document.getElementById("mini-screen");
+        console.log(guitar);
+        console.log(mainGuitar);
+    }
 
-    const guitarElement = document.getElementById("main-guitar");
     guitarElement.innerHTML = "";
 
     guitarAxisRow = getGuitarAxis(guitarElement);
 
     getGuitarStrings(guitar, guitarElement, guitarAxisRow, accidentalType);
 
-    addOpenEventListenersToGuitar();
+    if (screen === "main-guitar") {
+        addOpenEventListenersToGuitar();
+    }
 }
 
 
@@ -217,13 +242,13 @@ function initializePage() {
     footerPTag.append(footerText);
 
     createDropdown("guitar-tuning-dropdown", tuningList, {valueMethod: true});
-    createDropdown("guitar-fretNum-dropdown", [ 19, 20, 21, 22, 23, 24 ], {defaultOption: 22});
+    createDropdown("guitar-numOfFrets-dropdown", [ 19, 20, 21, 22, 23, 24 ], {defaultOption: 22});
     createDropdown("guitar-accidentalType-dropdown", [ "#", "b" ], {});
     const guitarApp = new GuitarApp({tuning: "standard", numOfFrets: 22});
     const cordList = Object.keys(guitarApp.cordDict);
     createDropdown("mini-screen-chord-dropdown", cordList, {textMethod: true});
     
-    updateGuitarDiv();
+    updateGuitarDiv("main-guitar", {});
 
     addCloseEventListenersToGuitar();
 }
