c6162ef77939960f1bf9eace5df68113620dbe5e
aaron <aaronliftig@gmail.com>
2022-09-03 21:07:00 +1000

    Fix incorrect note labels

--- js/miniScreenFunctions.js
+++ js/miniScreenFunctions.js
@@ -12,6 +12,28 @@ function applyNewChord() {
 }
 
 
+function extractNumber(text) {
+    return parseInt(text.match(/\d+/g));
+}
+
+
+function fixTextContent(text, previousText) {
+    if (previousText == null) return text;
+
+    let textNumber = extractNumber(text);
+    let previousTextNumber = extractNumber(previousText);
+
+    if (previousTextNumber > textNumber) {
+        textNumber += 7;
+        let firstWord = text.split(" ")[0];
+        return firstWord + " " + textNumber + "th";
+    }
+    else {
+        return text;
+    }
+}
+
+
 function createMiniScreenNoteColorLegend(guitarApp, chordName) {
     const chordDict = guitarApp.chordDict;
     const noteNumberList = chordDict[chordName];
@@ -28,10 +50,13 @@ function createMiniScreenNoteColorLegend(guitarApp, chordName) {
     const legendList = document.getElementById("chord-legend");
     legendList.innerHTML = "";
 
+    let previousText = null;
     for (let noteInfo of uniqueNoteNames) {
         const listItem = document.createElement("li");
         listItem.style.backgroundColor = noteInfo[1];
-        listItem.textContent = noteInfo[0];
+        let fixedText = fixTextContent(noteInfo[0], previousText);
+        previousText = fixedText;
+        listItem.textContent = fixedText;
         adjustTextColorToBackground(listItem, listItem.style.backgroundColor);
         legendList.append(listItem);
     }
--- js/modalFunctions.js
+++ js/modalFunctions.js
@@ -1,12 +1,12 @@
 function openModal(modal) {
-    if (modal == null) return
+    if (modal == null) return;
     modal.classList.add("active");
     overlay.classList.add("active");
 }
 
 
 function closeModal(modal) {
-    if (modal == null) return
+    if (modal == null) return;
     modal.classList.remove("active");
     overlay.classList.remove("active");
 }
